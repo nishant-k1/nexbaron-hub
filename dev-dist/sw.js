ce47b5404e5fb52bedbb79558cb407799aec24bb
Nishant Kumar <nishant.29k1@gmail.com>
2026-08-31 04:03:25 +0530

    Update service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e01a84se8o8"
+    "revision": "0.5u2l7jjdjj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
